4bc0e0ff1a5a52a1571c3980c2dd2c060625ee53
Sourav Tanwar <souravtanwar30@gmail.com>
2025-01-29 23:39:40 +0530

    add getcurrent user and update user endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -140,5 +140,22 @@ const refreshAccessToken = asyncHandler(async(req, res)=>{
     }
 })
 
+const getCurrentUser = asyncHandler(async(req, res)=>{
+    return res.status(200)
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
+})
 
-export {registerUser, loginUser, logOutUser, refreshAccessToken}
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {name, email, phoneNumber} = req.body
+
+    if(!name || !email || !phoneNumber ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = User.findByIdAndUpdate(req.user?._id, {$set: {name, email, phoneNumber}},{new:true}).select("-password -refreshToken")
+
+    return res.status(200)
+    .json(new ApiResponse(200, user, "Accout details updated successfully"))
+})
+
+export {registerUser, loginUser, logOutUser, refreshAccessToken, getCurrentUser, updateAccountDetails}
